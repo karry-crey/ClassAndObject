b810cf173e5e36b20de0908fc1dc0dae1e658323
karry-crey <848936808@qq.com>
2026-07-11 15:38:39 +0800

    添加项目文件。

---
+++ ClassAndObject/Test.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+class Date {
+public:
+	//initialize
+	//Date() {
+	//	_year = 1;
+	//	_month = 1;
+	//	_day = 1;
+	//}
+	//Date(int year, int month, int day) {
+	//	_year = year;
+	//	_month = month;
+	//	_day = day;
+	//}
+
+	void Print() {
+		cout << this->_year <<"/"<< this->_month <<"/"<< this->_day << endl;
+	}
+
+	//~Date() {
+	//	// in fact this class not need the function
+	//}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+int main() {
+	Date d1;
+	d1.Print();
+	//Date d2(2026, 7, 11);
+	//d2.Print();
+	return 0;
+}
