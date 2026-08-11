3ca66a9f8dc64f905eeb36505ba97ba2c4220a15
karry-crey <848936808@qq.com>
2026-08-11 14:51:54 +0800

    update

--- ClassAndObject/Test1.cpp
+++ ClassAndObject/Test1.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
+#include<iostream>
 #include<stdio.h>
 using namespace std;
 
@@ -11,10 +11,17 @@ public:
 		_day = day;
 	}
 
-	Date(Date& d) {
+	Date(const Date& d) {
 		_year = d._day;
 		_month = d._month;
 		_day = d._day;
+	}
+
+	//隐含了this 参数
+	bool operator==(Date& d) {
+		return _year == d._year
+			&& _month == d._month
+			&& _day == d._day;
 	}
 
 private:
@@ -23,11 +30,42 @@ private:
 	int _day;
 };
 
+//bool operator==(Date& d1, Date& d2) {
+//	return d1._year == d2._year
+//		&& d1._month == d2._month
+//		&& d1._day == d2._day;
+//}
 
+class A {
+public:
+	void func() {
+		cout << "A::func()" << endl;
+	}
+};
+
+void f() {
+	cout << "f()" << endl;
+}
+
+//int main() {
+//	Date d1(2025, 4, 24);//普通构造
+//	Date d2(d1); //拷贝构造
+//	Date d3 = d1;//拷贝构造
+//	Date d4(2025, 5, 26);
+//	cout << (d1 == d4) << endl;//加括号，<<优先级大于运算符
+//	//operator==(d1, d4);
+//	cout << d1.operator==(d4) << endl;
+//
+//	return 0;
+//}
 
 int main() {
-	Date d1(2025, 4, 24);//普通构造
-	Date d2(d1); //拷贝构造
-	Date d3 = d1;//拷贝构造
+	void(*func1)() = f;
+	(*func1)();
+
+	//成员函数
+	void(A:: * func2)() = &A::func;
+	A aa;
+	(aa.*func2)();
 	return 0;
 }
